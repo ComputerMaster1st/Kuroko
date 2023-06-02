a9ba18b826ad0778debb1ca67e452752f3f7e1de
ComputerMaster1st <24191511+ComputerMaster1st@users.noreply.github.com>
2023-06-02 16:42:16 +0100

    Minor change

--- NewNewRailgun/Core/CoreLogHeader.cs
+++ NewNewRailgun/Core/CoreLogHeader.cs
@@ -5,6 +5,6 @@ namespace NewNewRailgun.Core
     internal class CoreLogHeader : LogHeader
     {
         public const string SYSTEM = "SYSTEM";
-        public const string MODLOADER = "MOD-LOAD";
+        public const string MODLOADER = "MOD-LOADER";
     }
 }
