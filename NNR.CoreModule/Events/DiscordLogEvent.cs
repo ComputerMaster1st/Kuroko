c0bf0c04091654b97f77dcf879369fdd599dbadc
ComputerMaster1st <24191511+ComputerMaster1st@users.noreply.github.com>
2023-06-02 02:40:51 +0100

    Created DiscordLogEvent

---
+++ NNR.CoreModule/Events/DiscordLogEvent.cs
@@ -0,0 +1,17 @@
+﻿using Discord;
+using Discord.WebSocket;
+using NNR.MDK;
+using NNR.MDK.Attributes;
+
+namespace NNR.CoreModule.Events
+{
+    [PreInitialize]
+    public class DiscordLogEvent
+    {
+        public DiscordLogEvent(DiscordShardedClient discordClient)
+            => discordClient.Log += LogEvent;
+
+        private Task LogEvent(LogMessage logMessage)
+            => Utilities.WriteLogAsync(logMessage);
+    }
+}
