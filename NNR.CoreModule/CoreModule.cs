78a9beb1cb01910c4aeab284a01e0492595f7e24
ComputerMaster1st <24191511+ComputerMaster1st@users.noreply.github.com>
2023-06-02 03:48:16 +0100

    Added descriptors

--- NNR.CoreModule/CoreModule.cs
+++ NNR.CoreModule/CoreModule.cs
@@ -6,6 +6,10 @@ namespace NNR.CoreModule
 {
     public class CoreModule : INnrModule
     {
+        public string ModuleName => "New New Railgun: Core Module";
+        public string ModuleCodeName => "NNR_CORE";
+        public string ModuleDescription => "Contains primary events and basic commands.";
+
         public void RegisterToDependencyInjection(ref IServiceCollection serviceCollection)
         {
             serviceCollection.AddSingleton<DiscordLogEvent>();
--- NewNewRailgun/ModuleLoader.cs
+++ NewNewRailgun/ModuleLoader.cs
@@ -1,5 +1,4 @@
 ﻿using Discord;
-using Microsoft.Extensions.DependencyInjection;
 using NewNewRailgun.Core;
 using NNR.MDK;
 using System.Reflection;
@@ -8,26 +7,36 @@ namespace NewNewRailgun
 {
     internal class ModuleLoader
     {
-        private readonly List<Assembly> _modules = new();
+        private readonly List<Assembly> _moduleAssemblies = new();
+        private readonly List<INnrModule> _modules = new();
 
         public async Task<int> ScanForModulesAsync()
         {
+            var iModuleType = typeof(INnrModule);
+
             foreach (var dll in Directory.GetFiles(DataDirectories.MODULES))
             {
-                var module = Assembly.LoadFile(dll);
+                var moduleDll = Assembly.LoadFile(dll);
+
+                var moduleSetupType = moduleDll.GetTypes()
+                    .Where(iModuleType.IsAssignableFrom)
+                    .FirstOrDefault();
+
+                if (moduleSetupType is null)
+                {
+                    await Utilities.WriteLogAsync(new LogMessage(LogSeverity.Info, CoreLogHeader.SYSTEM, $"Failed to load: {moduleDll.FullName}! Missing \"INnrModule\". Contact Module Developer!"));
+                    continue;
+                }
+
+                var module = Activator.CreateInstance(moduleSetupType) as INnrModule;
 
                 _modules.Add(module);
+                _moduleAssemblies.Add(moduleDll);
 
-                await Utilities.WriteLogAsync(new LogMessage(LogSeverity.Info, CoreLogHeader.SYSTEM, "Found: " + module.FullName));
+                await Utilities.WriteLogAsync(new LogMessage(LogSeverity.Info, CoreLogHeader.SYSTEM, $"Found: {module.ModuleName}"));
             }
 
-            return _modules.Count;
-        }
-
-
-        public void LoadModuleEvents(IServiceCollection serviceCollection)
-        {
-
+            return _moduleAssemblies.Count;
         }
     }
 }
