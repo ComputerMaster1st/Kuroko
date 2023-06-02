23934d2ac380a0b0dea4423feeef034e7a23ef26
ComputerMaster1st <24191511+ComputerMaster1st@users.noreply.github.com>
2023-06-02 02:56:01 +0100

    Created core module

---
+++ NNR.CoreModule/CoreModule.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NNR.CoreModule.Events;
+using NNR.MDK;
+
+namespace NNR.CoreModule
+{
+    public class CoreModule : INnrModule
+    {
+        public void RegisterToDependencyInjection(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<DiscordLogEvent>();
+        }
+    }
+}
---
+++ NNR.MDK/INnrModule.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace NNR.MDK
+{
+    public interface INnrModule
+    {
+        void RegisterToDependencyInjection(IServiceCollection serviceCollection);
+    }
+}
