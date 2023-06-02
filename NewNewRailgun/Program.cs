ad2f44a20d4fa71a2c17ee8240b56c5b1c811355
ComputerMaster1st <24191511+ComputerMaster1st@users.noreply.github.com>
2023-06-02 01:42:39 +0100

    Added discord config check

--- NewNewRailgun/Program.cs
+++ NewNewRailgun/Program.cs
@@ -1,4 +1,13 @@
 ﻿using Discord;
+using NewNewRailgun.Core.Configuration;
 using NNR.MDK;
 
 await Utilities.WriteLogAsync(new LogMessage(LogSeverity.Info, LogHeader.SYSTEM, "Now Starting New New Railgun. Please wait a few minutes to boot the operating system..."));
+
+var discordConfig = await NnrDiscordConfig.LoadAsync();
+
+if (discordConfig is null)
+{
+    await Utilities.WriteLogAsync(new LogMessage(LogSeverity.Info, LogHeader.SYSTEM, "New \"nnr_discord_config.json\" file has been generated! Please fill this in before restarting the bot!"));
+    return;
+}
