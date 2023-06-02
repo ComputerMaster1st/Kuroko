7156a1382cac6b1f7ceb8655a42ce80a9767ad98
ComputerMaster1st <24191511+ComputerMaster1st@users.noreply.github.com>
2023-06-02 03:04:26 +0100

    Now scans for available modules

--- NewNewRailgun/ModuleLoader.cs
+++ NewNewRailgun/ModuleLoader.cs
@@ -1,7 +1,32 @@
-﻿namespace NewNewRailgun
+﻿using Discord;
+using Microsoft.Extensions.DependencyInjection;
+using NNR.MDK;
+using System.Reflection;
+
+namespace NewNewRailgun
 {
     internal class ModuleLoader
     {
+        private readonly List<Assembly> _modules = new();
 
+        public async Task<int> ScanForModulesAsync()
+        {
+            foreach (var dll in Directory.GetFiles(DataDirectories.MODULES))
+            {
+                var module = Assembly.LoadFile(dll);
+
+                _modules.Add(module);
+
+                await Utilities.WriteLogAsync(new LogMessage(LogSeverity.Info, LogHeader.SYSTEM, "Found: " + module.FullName));
+            }
+
+            return _modules.Count;
+        }
+
+
+        public void LoadModuleEvents(IServiceCollection serviceCollection)
+        {
+
+        }
     }
 }
