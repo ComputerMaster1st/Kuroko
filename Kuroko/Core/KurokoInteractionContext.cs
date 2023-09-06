a2268e9d7d42ef2221ebbd830068cc1fbe7d10e5
ComputerMaster1st <24191511+ComputerMaster1st@users.noreply.github.com>
2023-09-06 15:25:29 +0100

    Added database as property in context

--- Kuroko/Core/KurokoInteractionContext.cs
+++ Kuroko/Core/KurokoInteractionContext.cs
@@ -1,11 +1,24 @@
 ﻿using Discord.Interactions;
 using Discord.WebSocket;
+using Kuroko.Database;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Kuroko.Core
 {
     public class KurokoInteractionContext : ShardedInteractionContext
     {
+        private DatabaseContext _db = null;
+
         public IServiceProvider ServiceProvider { get; }
+
+        public DatabaseContext Database
+        {
+            get
+            {
+                _db ??= ServiceProvider.GetRequiredService<DatabaseContext>();
+                return _db;
+            }
+        }
 
         public KurokoInteractionContext(DiscordShardedClient client, SocketInteraction interaction, IServiceProvider serviceProvider) : base(client, interaction)
             => ServiceProvider = serviceProvider;
