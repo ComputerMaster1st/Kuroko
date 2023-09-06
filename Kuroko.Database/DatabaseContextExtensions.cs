7264078aa228ab291ff71ace9fc02e55a54df1ec
ComputerMaster1st <24191511+ComputerMaster1st@users.noreply.github.com>
2023-09-06 16:12:11 +0100

    Removed get method

--- Kuroko.Database/DatabaseContextExtensions.cs
+++ Kuroko.Database/DatabaseContextExtensions.cs
@@ -5,9 +5,6 @@ namespace Kuroko.Database
 {
     public static class DatabaseContextExtensions
     {
-        public static Task<TDiscordEntity> GetDataAsync<TDiscordEntity>(this DbSet<TDiscordEntity> dbSet, ulong id) where TDiscordEntity : class, IDiscordEntity
-            => dbSet.FirstOrDefaultAsync(x => x.Id == id);
-
         public static async Task<TDiscordEntity> CreateOrGetDataAsync<TDiscordEntity>(this DbSet<TDiscordEntity> dbSet, ulong id) where TDiscordEntity : class, IDiscordEntity
         {
             var entity = await dbSet.FirstOrDefaultAsync(x => x.Id == id);
