ad216c754cdba39f50a1b43dbc4d33d06561b821
ComputerMaster1st <24191511+ComputerMaster1st@users.noreply.github.com>
2023-09-06 16:57:40 +0100

    Added more attributes

--- Kuroko/Core/Attributes/KurokoEventAttribute.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace Kuroko.MDK.Attributes
-{
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    public class KurokoEventAttribute : Attribute { }
-}
---
+++ Kuroko/Core/Attributes/RequireUserGuildPermission.cs
@@ -0,0 +1,35 @@
+﻿using Discord;
+using Discord.Interactions;
+using Kuroko.Core.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Kuroko.Core.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+    public class RequireUserGuildPermission : PreconditionAttribute
+    {
+        private readonly GuildPermission _permission;
+
+        public RequireUserGuildPermission(GuildPermission permission)
+            => _permission = permission;
+
+        public override Task<PreconditionResult> CheckRequirementsAsync(IInteractionContext context, ICommandInfo commandInfo, IServiceProvider services)
+        {
+            var config = services.GetRequiredService<KDiscordConfig>();
+            var user = context.User as IGuildUser;
+
+            if (!(user.GuildPermissions.Has(_permission) ||
+                user.GuildPermissions.Administrator ||
+                config.BotAdminUserIds.Contains(user.Id) ||
+                user.Id == config.BotOwnerId))
+            {
+                return Task.FromResult(PreconditionResult.FromError(
+                    string.Format("{0} Missing {1} Server Permission!",
+                        Format.Bold("ACCESS DENIED:"),
+                        _permission)));
+            }
+
+            return Task.FromResult(PreconditionResult.FromSuccess());
+        }
+    }
+}
