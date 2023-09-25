7ccdba51a5f823cef2054fdf8acbe849c8989e64
ComputerMaster1st <24191511+ComputerMaster1st@users.noreply.github.com>
2023-09-25 16:03:13 +0100

    Added exit component

---
+++ Kuroko/Modules/Globals/ExitComponent.cs
@@ -0,0 +1,19 @@
+﻿using Discord.Interactions;
+using Kuroko.Core;
+
+namespace Kuroko.Modules.Globals
+{
+    // TODO: This will handle exiting of main menu
+
+    public class ExitComponent : KurokoModuleBase
+    {
+        [ComponentInteraction(CommandIdMap.Exit)]
+        public async Task ExecuteAsync()
+        {
+            await DeferAsync();
+
+            var msg = await Context.Interaction.GetOriginalResponseAsync();
+            await msg.DeleteAsync();
+        }
+    }
+}
