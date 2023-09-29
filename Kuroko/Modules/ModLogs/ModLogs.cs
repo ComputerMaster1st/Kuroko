ba51f77065f0c07303309fc4b091cb6c5ef114a2
ComputerMaster1st <24191511+ComputerMaster1st@users.noreply.github.com>
2023-09-29 16:15:50 +0100

    Started modlog slash command

---
+++ Kuroko/Modules/ModLogs/ModLogs.cs
@@ -0,0 +1,28 @@
+﻿using Discord;
+using Discord.Interactions;
+using Kuroko.Core;
+using Kuroko.Core.Attributes;
+using System.Text;
+
+namespace Kuroko.Modules.ModLogs
+{
+    [RequireUserGuildPermission(GuildPermission.ManageGuild)]
+    public class ModLogs : KurokoModuleBase
+    {
+        private StringBuilder OutputMsg
+        {
+            get
+            {
+                return new StringBuilder()
+                    .AppendLine("# Moderation Logging");
+            }
+        }
+
+
+        [SlashCommand("modlogs", "Configure moderation logging")]
+        public async Task EntryAsync()
+        {
+
+        }
+    }
+}
