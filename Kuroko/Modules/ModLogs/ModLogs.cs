2e1feb2a07c791903b34ca7629980632ea01f401
ComputerMaster1st <24191511+ComputerMaster1st@users.noreply.github.com>
2023-09-29 18:06:54 +0100

    Expanded slash command and added return method

--- Kuroko/Modules/ModLogs/ModLogs.cs
+++ Kuroko/Modules/ModLogs/ModLogs.cs
@@ -1,13 +1,13 @@
 ﻿using Discord;
 using Discord.Interactions;
-using Kuroko.Core;
 using Kuroko.Core.Attributes;
+using Kuroko.Services;
 using System.Text;
 
 namespace Kuroko.Modules.ModLogs
 {
     [RequireUserGuildPermission(GuildPermission.ManageGuild)]
-    public class ModLogs : KurokoModuleBase
+    public class ModLogs : ModLogBase
     {
         private StringBuilder OutputMsg
         {
@@ -22,7 +22,60 @@ namespace Kuroko.Modules.ModLogs
         [SlashCommand("modlogs", "Configure moderation logging")]
         public async Task EntryAsync()
         {
+            await ExecuteAsync();
+        }
 
+        [ComponentInteraction($"{CommandIdMap.ModLogMenu}:*")]
+        public async Task ReturningAsync(ulong interactedUserId)
+        {
+            if (interactedUserId != Context.User.Id)
+            {
+                await RespondAsync("You can not perform this action due to not being the original user.", ephemeral: true);
+                return;
+            }
+
+            await ExecuteAsync(true);
+        }
+
+        private async Task ExecuteAsync(bool isReturning = false)
+        {
+            var user = Context.User as IGuildUser;
+            var properties = await GetPropertiesAsync();
+            var msgComponents = MLMenu.BuildMenu(user, properties);
+            var logChannel = Context.Guild.GetChannel(properties.LogChannelId);
+            var channelTag = (logChannel is null) ? "**Not Set**" : $"<#{logChannel.Id}>";
+            var output = OutputMsg
+                .AppendLine($"Logging Channel: {channelTag}")
+                .AppendLine("## Ignored Channels");
+            var temp = new List<ulong>();
+
+            if (properties.IgnoredChannelIds.Count > 0)
+                foreach (var channelId in properties.IgnoredChannelIds)
+                {
+                    var channel = Context.Guild.GetChannel(channelId.Value);
+
+                    if (channel is null)
+                    {
+                        temp.Add(channelId.Value);
+                        continue;
+                    }
+
+                    output.AppendLine($"* <#{channel.Id}>");
+                }
+            else
+                output.AppendLine($"* **None set**");
+
+            if (!isReturning)
+            {
+                await RespondAsync(output.ToString(), components: msgComponents);
+                (await Context.Interaction.GetOriginalResponseAsync()).SetTimeout(1);
+            }
+            else
+                (await Context.Interaction.ModifyOriginalResponseAsync(x =>
+                {
+                    x.Content = output.ToString();
+                    x.Components = msgComponents;
+                })).ResetTimeout();
         }
     }
 }
