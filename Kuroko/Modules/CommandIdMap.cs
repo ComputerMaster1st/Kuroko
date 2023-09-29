0d893ab3a2265ff81c62f36a3ed019041be3aae1
ComputerMaster1st <24191511+ComputerMaster1st@users.noreply.github.com>
2023-09-29 18:02:59 +0100

    Updated command map for modlog

--- Kuroko/Modules/CommandIdMap.cs
+++ Kuroko/Modules/CommandIdMap.cs
@@ -34,5 +34,21 @@
         #endregion
 
         #endregion
+
+        #region ModLogs
+
+        public const string ModLogMenu = "ml_menu";
+        public const string MogLogChannel = "ml_channel";
+        public const string ModLogChannelSave = "ml_channel_save";
+        public const string ModLogChannelResume = "ml_channel_resume";
+        public const string ModLogChannelResumeSave = "ml_channel_resume_save";
+        public const string ModLogChannelIgnore = "ml_channel_ignore";
+        public const string ModLogChannelIgnoreSave = "ml_channel_ignore_save";
+        public const string ModLogMessageDeleted = "ml_message_deleted";
+        public const string ModLogMessageEdited = "ml_message_edited";
+        public const string ModLogJoin = "ml_join";
+        public const string ModLogLeave = "ml_leave";
+
+        #endregion
     }
 }
