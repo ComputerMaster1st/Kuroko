400a0577fc0c09137087de6bc216540d2b71d3cd
ComputerMaster1st <24191511+ComputerMaster1st@users.noreply.github.com>
2023-10-06 18:07:59 +0100

    Created AttachmentEntity

---
+++ Kuroko.Database/Entities/Message/AttachmentEntity.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Kuroko.Database.Entities.Message
+{
+    public class AttachmentEntity : IDiscordEntity
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public ulong Id { get; private set; } = 0;
+
+        public string FileName { get; private set; } = string.Empty;
+        public long FileSize { get; private set; } = 0;
+
+        public string Base64Bytes { get; private set; } = string.Empty;
+
+        public AttachmentEntity(ulong attachmentId, string fileName, byte[] bytes)
+        {
+            Id = attachmentId;
+            FileName = fileName;
+            Base64Bytes = Convert.ToBase64String(bytes);
+            FileSize = bytes.LongLength;
+        }
+    }
+}
