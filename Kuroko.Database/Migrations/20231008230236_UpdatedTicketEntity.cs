6442fda8074b4b104496095ce9a72602bedba50d
ComputerMaster1st <24191511+ComputerMaster1st@users.noreply.github.com>
2023-10-09 00:07:03 +0100

    Updated TicketEntity in database

---
+++ Kuroko.Database/Migrations/20231008230236_UpdatedTicketEntity.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kuroko.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedTicketEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "ChannelId",
+                table: "Tickets",
+                type: "numeric(20,0)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "SummaryMessageId",
+                table: "Tickets",
+                type: "numeric(20,0)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ChannelId",
+                table: "Tickets");
+
+            migrationBuilder.DropColumn(
+                name: "SummaryMessageId",
+                table: "Tickets");
+        }
+    }
+}
