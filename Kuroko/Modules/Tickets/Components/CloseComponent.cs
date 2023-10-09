c481f4c0d7380eb7c06a38c036161c8f3df54c45
ComputerMaster1st <24191511+ComputerMaster1st@users.noreply.github.com>
2023-10-09 01:45:29 +0100

    Added TODO and now removes ticket from database

--- Kuroko/Modules/Tickets/Components/CloseComponent.cs
+++ Kuroko/Modules/Tickets/Components/CloseComponent.cs
@@ -1,15 +1,22 @@
 ﻿using Discord;
 using Discord.Interactions;
 using Kuroko.Core;
+using Kuroko.Database;
 
 namespace Kuroko.Modules.Tickets.Components
 {
     public class CloseComponent : KurokoModuleBase
     {
-        [ComponentInteraction(TicketsCommandMap.CloseTicket)]
-        public async Task ExecuteAsync()
+        [ComponentInteraction($"{TicketsCommandMap.CloseTicket}:*")]
+        public async Task ExecuteAsync(int ticketId)
         {
-            var channel = Context.Interaction.Channel as ITextChannel;
+            // TODO: Close Ticket & present transcript + delete buttons
+            var root = await Context.Database.Guilds.GetOrCreateRootAsync(Context.Guild.Id);
+            var channel = Context.Channel as ITextChannel;
+            var ticket = Context.Database.Tickets.FirstOrDefault(x => x.Id == ticketId);
+
+            root.Tickets.Remove(ticket, Context.Database);
+
             await channel.DeleteAsync();
         }
     }
