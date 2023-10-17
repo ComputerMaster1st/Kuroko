18a17b8f16508674027f35e716fee7ff7e3489b6
ComputerMaster1st <24191511+ComputerMaster1st@users.noreply.github.com>
2023-10-17 18:56:29 +0100

    Created embed ReportedMessageBuilder

---
+++ Kuroko/Modules/Reports/ReportedMessageBuilder.cs
@@ -0,0 +1,19 @@
+﻿using Discord;
+
+namespace Kuroko.Modules.Reports
+{
+    public static class ReportedMessageBuilder
+    {
+        public static Embed Build(string content, DateTimeOffset timestamp)
+        {
+            var embedBuilder = new EmbedBuilder()
+            {
+                Color = Color.Magenta,
+                Timestamp = timestamp,
+                Description = content
+            };
+
+            return embedBuilder.Build();
+        }
+    }
+}
