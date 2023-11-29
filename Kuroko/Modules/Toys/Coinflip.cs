8f6e82922366124c9b8392f2b1ee6a8eb0e84f03
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2023-11-29 02:58:40 +0000

    Bugfix and tweaks

--- Kuroko/Modules/Toys/Coinflip.cs
+++ Kuroko/Modules/Toys/Coinflip.cs
@@ -1,7 +1,6 @@
 ﻿using Discord;
 using Discord.Interactions;
 using Kuroko.Core;
-using System.Text;
 
 namespace Kuroko.Modules.Toys
 {
@@ -9,29 +8,25 @@ namespace Kuroko.Modules.Toys
     {
         private readonly Random _random = new();
 
-        [SlashCommand("Coinflip", "Flip a coin.")]
-        public async Task ExecuteAsync(int rolls, int diceSize = 6)
+        [SlashCommand("coinflip", "Flip a coin.")]
+        public async Task ExecuteAsync()
         {
-            EmbedBuilder embed = new EmbedBuilder()
-                 .WithColor(Color.Red)
-                 .WithTitle("Coin");
+            var embed = new EmbedBuilder()
+            {
+                Color = Color.Red,
+                Title = "Coin"
+            };
 
             var result = _random.Next(2);
 
             if (result == 1)
-            {
-                embed.WithDescription("You Flipped.. HEADS!");
-                embed.WithImageUrl("https://i.imgur.com/Y77AMLp.png");
-            }
+                embed.WithDescription("You Flipped.. HEADS!")
+                    .WithImageUrl("https://i.imgur.com/Y77AMLp.png");
             else
-            {
-                embed.WithDescription("You Flipped.. TAILS!");
-                embed.WithImageUrl("https://i.imgur.com/O3ULvhg.png");
-            }
+                embed.WithDescription("You Flipped.. TAILS!")
+                    .WithImageUrl("https://i.imgur.com/O3ULvhg.png");
             
-            await ReplyAsync(embed: embed.Build());
-            
-                
+            await RespondAsync(embed: embed.Build());
         }
     }
 }
