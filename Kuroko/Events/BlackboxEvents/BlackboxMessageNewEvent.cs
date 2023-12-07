3ec3e0c90b340d6d6c773096d66a291d526d1d54
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2023-12-07 04:26:44 +0000

    Blackbox now checks if msg is part of ticket

--- Kuroko/Events/BlackboxEvents/BlackboxMessageNewEvent.cs
+++ Kuroko/Events/BlackboxEvents/BlackboxMessageNewEvent.cs
@@ -1,20 +1,25 @@
 ﻿using Discord;
 using Discord.WebSocket;
 using Kuroko.Core.Attributes;
+using Kuroko.Database;
 using Kuroko.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Kuroko.Events.BlackboxEvents
 {
     [PreInitialize, KurokoEvent]
     public class BlackboxMessageNewEvent
     {
+        private readonly IServiceProvider _services;
         private readonly DiscordShardedClient _client;
         private readonly BlackboxService _blackbox;
 
-        public BlackboxMessageNewEvent(DiscordShardedClient client, BlackboxService blackbox)
+        public BlackboxMessageNewEvent(DiscordShardedClient client, BlackboxService blackbox, IServiceProvider services)
         {
             _client = client;
             _blackbox = blackbox;
+            _services = services;
 
             _client.MessageReceived += (m) => Task.Factory.StartNew(() => MessageReceivedAsync(m));
         }
@@ -25,6 +30,19 @@ namespace Kuroko.Events.BlackboxEvents
 
             if (msg.Author.Id == _client.CurrentUser.Id)
                 return;
+            
+            var channel = msg.Channel as IGuildChannel;
+            var db = _services.GetRequiredService<DatabaseContext>();
+            var modLogProperties = await db.GuildModLogs.FirstOrDefaultAsync(x => x.GuildId == channel.GuildId);
+
+            if (modLogProperties is null || !modLogProperties.EnableBlackbox)
+                return;
+            
+            var root = await db.Guilds.GetDataAsync(channel.GuildId);
+
+            foreach (var ticket in root.Tickets)
+                if (ticket.ChannelId == channel.Id)
+                    return;
 
             await _blackbox.StoreMessageAsync(msg);
         }
