40f9d59feb47edda831a45df4a95e854df9c2946
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2023-12-07 02:17:14 +0000

    Creates embed with summary and attach to msg

--- Kuroko/Services/TicketService.cs
+++ Kuroko/Services/TicketService.cs
@@ -15,9 +15,7 @@ namespace Kuroko.Services
         private readonly IServiceProvider _services;
 
         public TicketService(IServiceProvider services)
-        {
-            _services = services;
-        }
+            => _services = services;
 
         public async Task BuildAndSendTranscriptAsync(ReportsEntity properties, IGuild guild, ITextChannel reportChannel,
             ITextChannel transcriptChannel, int ticketId)
@@ -27,35 +25,131 @@ namespace Kuroko.Services
             var ticket = await db.Tickets.FirstOrDefaultAsync(x => x.Id == ticketId);
             var handler = properties.ReportHandlers.FirstOrDefault(x => x.Id == ticket.ReportHandlerId);
             var directory = Directory.CreateDirectory($"{DataDirectories.TEMPFILES}/ticket_{ticket.Id}");
+            var reportedUser = await guild.GetUserAsync(ticket.ReportedUserId);
 
             await File.WriteAllTextAsync(Path.Combine(directory.ToString(), "ticket.txt"),
-                await GenerateTranscriptAsync(ticket, handler, guild));
+                await GenerateReportTranscriptAsync(ticket, handler, guild));
             var (ZipDir, Segments, Message) = await Utilities.ZipAndUploadAsync(ticket, directory, transcriptChannel);
 
             directory.Delete(true);
             ZipDir.Delete(true);
             root.Tickets.Remove(ticket, db);
 
-            await reportChannel.DeleteAsync();
-            await Message.ModifyAsync(x =>
+            var embedFieldList = new List<EmbedFieldBuilder>()
             {
-                x.Content = $"Ticket Transcript: {ticket.Id}. Uploaded {Segments} zip file(s).";
+                new()
+                {
+                    IsInline = true,
+                    Name = "Accused",
+                    Value = reportedUser is null ? ticket.ReportedUserId : reportedUser.Mention
+                },
+                new()
+                {
+                    IsInline = true,
+                    Name = "Severity",
+                    Value = ticket.Severity
+                },
+                new()
+                {
+                    IsInline = false,
+                    Name = "Rules Violated",
+                    Value = ticket.RulesViolated
+                },
+                new()
+                {
+                    IsInline = false,
+                    Name = "Subject",
+                    Value = ticket.Subject
+                }
+            };
+
+            await reportChannel.DeleteAsync();
+            await Message.ModifyAsync(async x =>
+            {
+                x.Embed = await GenerateEmbedAsync(ticket, guild, handler);
             });
         }
 
-        private async Task<string> GenerateTranscriptAsync(TicketEntity ticket, ReportHandler handler, IGuild guild)
+        private static async Task<Embed> GenerateEmbedAsync(TicketEntity ticket, IGuild guild, ReportHandler handler,
+            List<EmbedFieldBuilder> additionalFields = null)
+        {
+            var submitter = await guild.GetUserAsync(ticket.SubmitterId);
+            var usersInvolved = new Dictionary<IGuildUser, int>();
+
+            foreach (var msg in ticket.Messages)
+            {
+                var user = await guild.GetUserAsync(msg.UserId);
+
+                if (!usersInvolved.TryAdd(user, 1))
+                    usersInvolved[user]++;
+            }
+
+            var outputUsersInvolved = new StringBuilder();
+            foreach (var user in usersInvolved)
+                outputUsersInvolved.AppendLine($"{user.Value} - {user.Key.Mention}");
+
+            var fieldBuilders = new List<EmbedFieldBuilder>()
+            {
+                new()
+                {
+                    IsInline = true,
+                    Name = "Submitter",
+                    Value = submitter.Mention
+                },
+                new()
+                {
+                    IsInline = true,
+                    Name = "Created",
+                    Value = ticket.CreatedAt
+                },
+                new()
+                {
+                    IsInline = true,
+                    Name = "Resolved",
+                    Value = DateTimeOffset.UtcNow
+                },
+                new()
+                {
+                    IsInline = true,
+                    Name = "Users In Ticket",
+                    Value = outputUsersInvolved.ToString()
+                },
+                new()
+                {
+                    IsInline = true,
+                    Name = "Handler",
+                    Value = handler.Name
+                }
+            };
+
+            if (additionalFields != null)
+                fieldBuilders.AddRange(additionalFields);
+
+            var embedBuilder = new EmbedBuilder()
+            {
+                Title = $"Ticket ID: {ticket.Id}",
+                ThumbnailUrl = submitter.GetDisplayAvatarUrl(),
+                Timestamp = DateTimeOffset.UtcNow,
+                Description = ticket.Description ?? "_None Provided_",
+                Fields = fieldBuilders
+            };
+
+            return embedBuilder.Build();
+        }
+
+        private async Task<string> GenerateReportTranscriptAsync(TicketEntity ticket, ReportHandler handler, IGuild guild)
         {
             var db = _services.GetRequiredService<DatabaseContext>();
             var reportedUser = await guild.GetUserAsync(ticket.ReportedUserId);
             var output = new StringBuilder()
                 .AppendLine("############################################")
-                .AppendLine("## TICKET TRANSCRIPT! PLEASE DO NOT EDIT! ##")
+                .AppendLine("## REPORT TRANSCRIPT! PLEASE DO NOT EDIT! ##")
                 .AppendLine("############################################")
                 .AppendLine()
                 .AppendLine("/// SUMMARY")
                 .AppendLine()
                 .AppendLine("- CREATED AT (UTC) : " + ticket.CreatedAt.UtcDateTime)
-                .AppendLine("- CLOSED AT (UTC)  : " + DateTime.UtcNow)
+                .AppendLine("- CLOSED AT (UTC)  : " + DateTimeOffset.UtcNow)
                 .AppendLine("- SUBJECT          : " + ticket.Subject)
                 .AppendLine("- ACCUSED          : " + (reportedUser is null ? ticket.ReportedUserId : (reportedUser.GlobalName ?? reportedUser.Username)))
                 .AppendLine("- RULES VIOLATED   : " + ticket.RulesViolated)
