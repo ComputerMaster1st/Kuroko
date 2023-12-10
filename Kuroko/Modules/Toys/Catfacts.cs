d59f34ac80d15f1e7d7733a72c2a2fb9e23e04ed
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2023-12-10 20:10:35 +0000

    Little cleanup

--- Kuroko/Modules/Toys/Catfacts.cs
+++ Kuroko/Modules/Toys/Catfacts.cs
@@ -1,32 +1,23 @@
-﻿using Discord;
-using Discord.Interactions;
+﻿using Discord.Interactions;
 using Kuroko.Core;
 using Newtonsoft.Json;
-using System.Net;
 
 namespace Kuroko.Modules.Toys
 {
     public class Catfacts : KurokoModuleBase
     {
-        
         private readonly HttpClient _httpClient = new() 
         {
             BaseAddress = new Uri("https://catfact.ninja")
         };
 
         [SlashCommand("catfacts", "Gets a random cat fact.")]
-
         public async Task ExecuteAsync()
         {
             var webRequest = await _httpClient.GetStringAsync("/fact");
-            var json = JsonConvert.DeserializeObject<Dictionary<string,object>>(webRequest);
-            var result = json["fact"].ToString();
+            var result = JsonConvert.DeserializeObject<Dictionary<string,object>>(webRequest);
 
-                await RespondAsync(result);
-                   
-               
-
-           
+            await RespondAsync(result["fact"].ToString());
         }
     }
 }
