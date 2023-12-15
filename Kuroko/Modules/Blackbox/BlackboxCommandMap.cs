f29597cc95d0613db5acd339d0781489b5ce1f05
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2023-12-15 14:31:11 +0000

    moved map properties

--- Kuroko/Modules/Blackbox/BlackboxCommandMap.cs
+++ Kuroko/Modules/Blackbox/BlackboxCommandMap.cs
@@ -5,5 +5,8 @@ namespace Kuroko.Modules.Blackbox
         private const string PREFIX = "blackbox_";
 
         public const string MENU = PREFIX + "menu";
+
+        public const string ENABLE = PREFIX + "enable";
+        public const string ATTACHMENTS = PREFIX + "attachments";
     }
 }
