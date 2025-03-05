e394c1c6329fd3450dc83d996a4d90a97689449d
Nekoputer <mr_m.a_thompson@hotmail.co.uk>
2025-03-05 20:57:58 +0100

    Expanded summary

--- Kuroko.Shared/BanSyncMode.cs
+++ Kuroko.Shared/BanSyncMode.cs
@@ -3,14 +3,24 @@ namespace Kuroko.Shared;
 public enum BanSyncMode
 {
     /// <summary>
-    /// BanSyncMode
-    /// Disabled: No Sync
-    /// Simplex: Read-Only
-    /// HalfDuplex: Read & Send Warnings
-    /// FullDuplex: Read & Send Warnings & Bans
+    /// No Sync
     /// </summary>
     Disabled,
+    /// <summary>
+    /// Host Default Setting
+    /// </summary>
+    Default,
+    /// <summary>
+    /// Read Host Banlist Only
+    /// </summary>
     Simplex,
+    /// <summary>
+    /// Read Host Banlist & Send Warnings To Host
+    /// </summary>
     HalfDuplex,
+    /// <summary>
+    /// Read Host Banlist & Send Warnings To Host & Execute Ban On Host
+    /// WARNING: ONLY TO BE USED FOR GROUPED COMMUNITY SERVERS
+    /// </summary>
     FullDuplex
 }
