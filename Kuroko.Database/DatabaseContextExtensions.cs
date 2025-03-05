6ad75bc6b0ad4057da6babe061ec67305183f6fd
Nekoputer <mr_m.a_thompson@hotmail.co.uk>
2025-03-05 21:24:32 +0100

    Enabled properties extension from database

--- Kuroko.Database/DatabaseContextExtensions.cs
+++ Kuroko.Database/DatabaseContextExtensions.cs
@@ -17,4 +17,27 @@ public static class DatabaseContextExtensions
         await dbTable.AddAsync(data);
         return data;
     }
+    
+    public static async Task<TGuildPropertyEntity> CreateOrGetPropertiesAsync<TGuildPropertyEntity, TDiscordEntity>(
+        this DbSet<TGuildPropertyEntity> dbProperty, 
+        DbSet<TDiscordEntity> dbDiscordEntity, 
+        ulong guildId,
+        Action<TDiscordEntity, TGuildPropertyEntity> guildEntityAction)
+        where TGuildPropertyEntity : class, IPropertyEntity
+        where TDiscordEntity : class, IDiscordEntity
+    {
+        var propertyEntity = await dbProperty.FirstOrDefaultAsync(x => x.GuildId == guildId);
+
+        if (propertyEntity != null)
+            return propertyEntity;
+
+        propertyEntity = Activator.CreateInstance<TGuildPropertyEntity>();
+
+        await dbProperty.AddAsync(propertyEntity);
+
+        var discordEntity = await dbDiscordEntity.GetOrCreateDataAsync(guildId);
+        guildEntityAction(discordEntity, propertyEntity);
+
+        return propertyEntity;
+    }
 }
--- Kuroko/KurokoCommandBase.cs
+++ Kuroko/KurokoCommandBase.cs
@@ -18,22 +18,22 @@ public abstract class KurokoCommandBase : InteractionModuleBase<KurokoInteractio
         await base.AfterExecuteAsync(command);
     }
     
-    // protected async Task<TPropertyEntity> GetPropertiesAsync<TPropertyEntity, TDiscordEntity>(ulong rootId)
-    //     where TPropertyEntity : class, IPropertyEntity
-    //     where TDiscordEntity : class, IDiscordEntity
-    // {
-    //     var set = Context.Database.Set<TPropertyEntity>();
-    //     var rootSet = Context.Database.Set<TDiscordEntity>();
-    //     var result = await set.CreateOrGetPropertiesAsync(rootSet, rootId, (x, y) =>
-    //     {
-    //         var properties = x.GetType().GetProperties();
-    //         var property = properties.FirstOrDefault(x => x.PropertyType == typeof(TPropertyEntity));
-    //
-    //         property.SetValue(x, y);
-    //     });
-    //
-    //     return result;
-    // }
+    protected async Task<TPropertyEntity> GetPropertiesAsync<TPropertyEntity, TDiscordEntity>(ulong rootId)
+        where TPropertyEntity : class, IPropertyEntity
+        where TDiscordEntity : class, IDiscordEntity
+    {
+        var set = Context.Database.Set<TPropertyEntity>();
+        var rootSet = Context.Database.Set<TDiscordEntity>();
+        var result = await set.CreateOrGetPropertiesAsync(rootSet, rootId, (x, y) =>
+        {
+            var properties = x.GetType().GetProperties();
+            var property = properties.FirstOrDefault(info => info.PropertyType == typeof(TPropertyEntity));
+    
+            property?.SetValue(x, y);
+        });
+    
+        return result;
+    }
     
     // protected async Task<TTicketEntity> GetTicketAsync<TTicketEntity>(int ticketId)
     //     where TTicketEntity : class, ITicketEntity
