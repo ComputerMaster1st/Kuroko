da8a900240cf7dd970ce67c0abd26056f39c3da3
Nekoputer <mr_m.a_thompson@hotmail.co.uk>
2025-03-06 22:06:22 +0100

    Implemented perm-based exit

--- Kuroko/Commands/ExitComponent.cs
+++ Kuroko/Commands/ExitComponent.cs
@@ -1,16 +1,25 @@
+using Discord;
 using Discord.Interactions;
+using Kuroko.Attributes;
 
 namespace Kuroko.Commands;
 
 public class ExitComponent : KurokoCommandBase
 {
-    [ComponentInteraction($"{CommandMap.EXIT}:*")]
+    [ComponentInteraction($"{CommandMap.EXIT_WITH_UID}:*")]
     public async Task ExecuteAsync(ulong interactedUserId)
     {
         if (interactedUserId != Context.User.Id)
             return;
-        await DeferAsync();
+        await ExecuteAsync();
+    }
 
+    [ComponentInteraction($"{CommandMap.EXIT_WITH_PERM}")]
+    [KurokoUserPermission(GuildPermission.ManageGuild)]
+    public async Task ExecuteAsync()
+    {
+        await DeferAsync();
+        
         var msg = await Context.Interaction.GetOriginalResponseAsync();
         if (msg != null)
             await msg.DeleteAsync();
