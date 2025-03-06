72c9e4678ffba3408ff194990ff028e8a17bd1ee
Nekoputer <mr_m.a_thompson@hotmail.co.uk>
2025-03-06 05:50:26 +0100

    Added RequireUserPermission.cs

---
+++ Kuroko/Attributes/RequireUserPermission.cs
@@ -0,0 +1,26 @@
+using Discord;
+using Discord.Interactions;
+using Kuroko.Shared;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Kuroko.Attributes;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+public class RequireUserPermission(GuildPermission permission) : PreconditionAttribute
+{
+    public override Task<PreconditionResult> CheckRequirementsAsync(
+        IInteractionContext context, ICommandInfo commandInfo, IServiceProvider services)
+    {
+        var config = services.GetRequiredService<KurokoConfig>();
+        var user = context.User as IGuildUser;
+
+        if (!(user!.GuildPermissions.Has(permission) ||
+              user.GuildPermissions.Administrator ||
+              config.AdminUserIds.Contains(user.Id) ||
+              user.Id == config.OwnerId))
+            return Task.FromResult(PreconditionResult.FromError(
+                $"{Format.Bold("ACCESS DENIED:")} Missing {permission} Server Permission!"));
+
+        return Task.FromResult(PreconditionResult.FromSuccess());
+    }
+}
