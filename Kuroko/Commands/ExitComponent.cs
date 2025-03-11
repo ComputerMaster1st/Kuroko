d1a24cd6eb3fdf19220d4196ac20dd69df8b5fd5
Nekoputer <mr_m.a_thompson@hotmail.co.uk>
2025-03-11 21:24:44 +0100

    Added exit bansync

--- Kuroko/Commands/ExitComponent.cs
+++ Kuroko/Commands/ExitComponent.cs
@@ -7,16 +7,21 @@ namespace Kuroko.Commands;
 public class ExitComponent : KurokoCommandBase
 {
     [ComponentInteraction($"{CommandMap.EXIT_WITH_UID}:*")]
-    public async Task ExecuteAsync(ulong interactedUserId)
-    {
-        if (interactedUserId != Context.User.Id)
-            return;
-        await ExecuteAsync();
-    }
+    public Task ExitWithUidAsync(ulong interactedUserId)
+        => !IsInteractedUser(interactedUserId) ? Task.CompletedTask : ExitAsync();
+
 
     [ComponentInteraction($"{CommandMap.EXIT_WITH_PERM}")]
     [KurokoUserPermission(GuildPermission.ManageGuild)]
-    public async Task ExecuteAsync()
+    public Task ExitWithManageGuildPermAsync()
+        => ExitAsync();
+
+    [ComponentInteraction($"{CommandMap.BANSYNC_IGNORE}")]
+    [KurokoUserPermission(GuildPermission.BanMembers)]
+    public Task ExitWithBanPermAsync()
+        => ExitAsync();
+    
+    private async Task ExitAsync()
     {
         await DeferAsync();
         
@@ -24,4 +29,5 @@ public class ExitComponent : KurokoCommandBase
         if (msg != null)
             await msg.DeleteAsync();
     }
+    
 }
