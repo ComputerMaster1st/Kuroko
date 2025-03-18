5e6fce957cf8162585ea472f43682a5787a59d17
Nekoputer <mr_m.a_thompson@hotmail.co.uk>
2025-03-18 23:42:28 +0100

    Changed memberships output and added count

--- Kuroko/Services/PatreonService.cs
+++ Kuroko/Services/PatreonService.cs
@@ -5,26 +5,42 @@ using Patreon.Net.Models;
 
 namespace Kuroko.Services;
 
-//[PreInitialize, KurokoDependency]
+[PreInitialize, KurokoDependency]
 public class PatreonService(KurokoConfig config) : IKurokoService
 {
     private PatreonClient _client = null;
 
-    public Task StartServiceAsync()
+    public async Task StartServiceAsync()
     {
         RefreshClient();
-        return Task.CompletedTask;
+
+        var campaigns = await _client.GetCampaignsAsync(Includes.All);
+        await foreach (var campaign in campaigns)
+            Console.WriteLine($"{campaign.Id} has {campaign.PatronCount} patrons");
     }
 
-    public Task<PatreonResourceArray<Member, MemberRelationships>> GetMembershipsAsync()
-        => _client.GetCampaignMembersAsync(config.PatreonCampaignId);
+    public async Task<IDictionary<Member, MemberRelationships>> GetMembershipsAsync()
+    {
+        var members = new Dictionary<Member, MemberRelationships>();
+        var incomingMembers = await _client.GetCampaignMembersAsync(config.PatreonCampaignId);
+        if (incomingMembers == null) return members;
+
+        await foreach (var member in incomingMembers)
+            members.Add(member, member.Relationships);
+
+        return members;
+    }
+
+    public async Task<int> CountMembersAsync()
+        => (await GetMembershipsAsync())
+            .Count(x => x.Key.PatronStatus == Member.PatronStatusValue.ActivePatron);
 
     public async Task<Member> GetMemberAsync(ulong discordUserId)
     {
         var memberships = await GetMembershipsAsync();
 
-        return await memberships.FirstOrDefaultAsync(x => 
-            x.Relationships.User.SocialConnections.Discord.UserId == discordUserId);
+        return memberships.FirstOrDefault(x => 
+            x.Value.User.SocialConnections.Discord.UserId == discordUserId).Key;
     }
 
     private void RefreshClient()
