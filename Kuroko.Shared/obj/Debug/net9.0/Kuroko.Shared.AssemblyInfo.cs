b354339fc5edf538326b4ac2fce9224770bf9226
Nekoputer <mr_m.a_thompson@hotmail.co.uk>
2025-03-18 18:44:03 +0100

    Installed FluentScheduler

--- Kuroko.Shared/obj/Debug/net9.0/Kuroko.Shared.AssemblyInfo.cs
+++ Kuroko.Shared/obj/Debug/net9.0/Kuroko.Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kuroko.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18f7b0c397ec8ef3a098481c7d6bd193f9ceb38e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12408246dbc12b48f48c573bffdacc9066765620")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kuroko.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kuroko.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
