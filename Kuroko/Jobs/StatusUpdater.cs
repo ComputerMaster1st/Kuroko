4bf454f0eacdd7e5d51e96ad14065f575360a059
Nekoputer <mr_m.a_thompson@hotmail.co.uk>
2025-03-18 23:38:50 +0100

    Included patron count in status

--- Kuroko/Jobs/StatusUpdater.cs
+++ Kuroko/Jobs/StatusUpdater.cs
@@ -2,43 +2,48 @@ using Discord;
 using Discord.WebSocket;
 using FluentScheduler;
 using Kuroko.Attributes;
+using Kuroko.Services;
 
-namespace Kuroko.Jobs
+namespace Kuroko.Jobs;
+
+[PreInitialize, KurokoJob]
+public class StatusUpdater(DiscordShardedClient client, PatreonService patreonService) : IJob, IScheduleJob
 {
-    [PreInitialize, KurokoJob]
-    public class StatusUpdater(DiscordShardedClient client) : IJob, IScheduleJob
-    {
-        private const string NAME = "Status Updater";
+    private const string NAME = "Status Updater";
 
-        public int PreviousServerCount { get; set; } = 0;
+    private int _previousServerCount = 0;
+    private int _previousPatronCount = 0;
 
-        public void Execute()
-            => ExecuteAsync().GetAwaiter();
+    public void Execute()
+        => ExecuteAsync().GetAwaiter();
 
-        public void ScheduleJob(Registry registry)
-            => registry.Schedule(this)
-                .WithName(NAME)
-                .ToRunEvery(30)
-                .Minutes();
+    public void ScheduleJob(Registry registry)
+        => registry.Schedule(this)
+            .WithName(NAME)
+            .ToRunEvery(30)
+            .Minutes();
         
-        public async Task ExecuteAsync()
+    private async Task ExecuteAsync()
+    {
+        await Utilities.WriteLogAsync(new LogMessage(LogSeverity.Info, LogHeader.JOBS, 
+            $"{NAME}: Job started at {DateTimeOffset.UtcNow}"));
+
+        var currentServerCount = client.Guilds.Count;
+        var currentPatronCount = await patreonService.CountMembersAsync();
+
+        if (currentServerCount == _previousServerCount || currentPatronCount == _previousPatronCount)
         {
-            await Utilities.WriteLogAsync(new LogMessage(LogSeverity.Info, LogHeader.JOBS, $"{NAME}: Job started at {DateTimeOffset.UtcNow}"));
-
-            var currentCount = client.Guilds.Count;
-
-            if (currentCount == PreviousServerCount)
-            {
-                await Utilities.WriteLogAsync(new LogMessage(LogSeverity.Info, LogHeader.JOBS,
-                    $"{NAME}: Job finished at {DateTimeOffset.UtcNow} <> No change in guild count."));
-                return;
-            }
-
-            PreviousServerCount = currentCount;
-
-            await client.SetGameAsync($"Prefix \"/\" {Utilities.SepChar} Guilds: {client.Guilds.Count}");
             await Utilities.WriteLogAsync(new LogMessage(LogSeverity.Info, LogHeader.JOBS,
-                $"{NAME}: Job finished at {DateTimeOffset.UtcNow} <> Guild count updated to {currentCount}!"));
+                $"{NAME}: Job finished at {DateTimeOffset.UtcNow} <> No status updates."));
+            return;
         }
+
+        _previousServerCount = currentServerCount;
+        _previousPatronCount = currentPatronCount;
+
+        await client.SetGameAsync($"Prefix \"/\" {Utilities.SepChar} Guilds: {currentServerCount} {
+            Utilities.SepChar} Patrons: {currentPatronCount}");
+        await Utilities.WriteLogAsync(new LogMessage(LogSeverity.Info, LogHeader.JOBS,
+            $"{NAME}: Job finished at {DateTimeOffset.UtcNow} <> Status Updated!"));
     }
 }
