3ad1989a6add7e8e6d7550df49b1be482b22a796
Nekoputer <mr_m.a_thompson@hotmail.co.uk>
2025-03-19 22:17:36 +0100

    Implemented redeem and revoke

--- Kuroko/Commands/Patreon/Patreon.cs
+++ Kuroko/Commands/Patreon/Patreon.cs
@@ -1,16 +1,14 @@
 using System.Text;
 using Discord.Interactions;
+using Kuroko.AutoCompletes;
 using Kuroko.Database.UserEntities;
+using Kuroko.Database.UserEntities.Extras;
 using Kuroko.Services;
 
 namespace Kuroko.Commands.Patreon;
 
 public class Patreon(PatreonService patreonService) : KurokoCommandBase
 {
-    // redeem command
-    // revoke command
-    // Check Key command
-
     [SlashCommand("patreon-status", "Check your patron status")]
     public async Task StatusAsync()
     {
@@ -35,5 +33,46 @@ public class Patreon(PatreonService patreonService) : KurokoCommandBase
                 membership.WillPayAmountCents / 100.00)}");
         
         await RespondAsync(output.ToString(), ephemeral: true);
+    }
+    
+    [SlashCommand("patreon-redeem", "Redeem a key on this server")]
+    public async Task RedeemAsync([Autocomplete(typeof(PatreonKeyRedeemAutocomplete))] int keyId)
+    {
+        var properties = await GetPropertiesAsync<PatreonProperties, UserEntity>(Context.User.Id);
+        PremiumKey key;
+        
+        if (keyId == -1 && (properties.KeysAllowed == -1 || 
+            (properties.KeysAllowed > 0 && properties.PremiumKeys.Count < properties.KeysAllowed)))
+        {
+            key = new PremiumKey();
+            properties.PremiumKeys.Add(key);
+        }
+        else
+            key = properties.PremiumKeys.FirstOrDefault(p => p.Id == keyId);
+        if (key is null)
+        {
+            await RespondAsync("**ERROR:** Invalid Premium Key!", ephemeral: true);
+            return;
+        }
+        
+        key.GuildId = Context.Guild.Id;
+        await RespondAsync("**SUCCESS:** Premium Key has been installed for this server! " +
+                           "The server hamsters are dancing with joy!", ephemeral: true);
+    }
+    
+    [SlashCommand("patreon-revoke", "Remove a key from a server to re-use somewhere else")]
+    public async Task RevokeAsync([Autocomplete(typeof(PatreonKeyRevokeAutocomplete))] int keyId)
+    {
+        var properties = await GetPropertiesAsync<PatreonProperties, UserEntity>(Context.User.Id);
+        var key = properties.PremiumKeys.FirstOrDefault(p => p.Id == keyId);
+        
+        if (key is null)
+        {
+            await RespondAsync("**ERROR:** Invalid Premium Key!", ephemeral: true);
+            return;
+        }
+        
+        key.GuildId = 0;
+        await RespondAsync("**SAD NOISES:** The server hamsters are sad now. Maybe next time...", ephemeral: true);
     }
 }
